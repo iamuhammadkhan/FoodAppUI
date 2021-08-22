67d8c842f3112e58c7286e4d847c041c2a9138a2
iamuhammadkhan <19494818+iamuhammadkhan@users.noreply.github.com>
2021-08-22 14:56:06 +0500

    Initial Commit

---
+++ FoodAppUI/FoodAppUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodAppUIApp.swift
+//  FoodAppUI
+//
+//  Created by Muhammad Khan on 8/22/21.
+//
+
+import SwiftUI
+
+@main
+struct FoodAppUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
